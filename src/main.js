b6b58df3642504e316b0ee5f53ce54c276fe7d3e
Qinhua_Zhao@outlook.com <xx9789710624>
2018-07-16 23:16:42 +0800

    项目代码初始化

--- src/main.js
+++ src/main.js
@@ -3,13 +3,30 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// reset.css 重置页面样式表,统一不同设备的浏览器初始化样式
+import './assets/styles/reset.css'
+//  解决一像素边框问题, 1px指一个css像素,但实际可能显示成多个物理像素, 在多倍屏里面亿像素边框被显示成多像素问题, 1px像素边框解决方案
+import './assets/styles/border.css'
+// 不同机型可能会出现300毫秒点击延迟问题  fastclick库 在项目目录下执行命令:  npm install fastclick --save  ; --save表示开发生产环境都需要,并会把它存到项目的package.json的dependencies中
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+//  使用fastClick
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  // router: router的简写, 导入是为了给子组件使用 <!--<router-view></router-view>-->
   router,
   components: { App },
   template: '<App/>'
 })
+
+// 路由就是根据网址的的不同,返回给用户不同的内容
+
+//  单页面应用VS多页面应用 鉴别: Network下的doc选项,看是否返回html
+//  多页面优点: 1.页面由服务器返回,一次请求html就可以了   2.首屏时间快, SEO(搜索引擎优化只认识html,不认识js)效果高.  缺点:页面切换慢
+//  单页面有点: 1.通过js感知url变化,然后卸载挂载,页面切换不需要html请求   缺点:首屏时间慢,需要请求html和js 可以通过服务器端渲染解决 Vue项目是反页面应用
+
+//  该项目是移动端项目
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!--<div class="home">Hello World</div>-->
+    <!--Vue一般不用a标签, 用router-link标签 to指向路由配置的地址-->
+    <!--<router-link class="home" to="/list">list</router-link>-->
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+/*.home {
+ font-size: 50px;
+}*/
+</style>
